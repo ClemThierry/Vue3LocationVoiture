b08f71b192da9810e52076da29652e7fb7078832
Clemence <clemence.thierry60@orange.fr>
2022-07-27 14:06:49 +0200

    Organize files

---
+++ FRONT/src/components/atoms/LinkNav.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <a class="text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md font-medium text-sm" v-bind:href="link">
+        {{text}}
+    </a>
+                    <!-- <a class="text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md font-medium text-base block " href="/#">
+                    Contact
+                </a> -->
+</div>
+</template>
+
+<script>
+export default {
+    name: 'LinkNav',
+    props: {
+        text: String,
+        link: {
+            default: "#",
+            type: String
+        }
+}
+}
+</script>
